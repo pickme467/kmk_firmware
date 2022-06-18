aa6c3df2e9a86c45bd27e9d39a2fa64e348913fe
pickme467 <pawel2267@gmail.com>
2022-06-19 00:21:19 +0200

    Gherkin no longer is time-dependent: new layers for switching keys.

--- user_keymaps/pickme467/gherkin-rp2040-zero/main.py
+++ user_keymaps/pickme467/gherkin-rp2040-zero/main.py
@@ -52,6 +52,7 @@ NUMBERS = 1
 RALTS = 2
 FUNCTIONS = 3
 STRINGS = 4
+MOD = 5
 
 # Tap Dance
 TD_UPPER_LOWER_MOD = KC.TD(
@@ -59,6 +60,8 @@ TD_UPPER_LOWER_MOD = KC.TD(
     KC.MO(FUNCTIONS),
     KC.MO(STRINGS)
     )
+
+FN_MOD = KC.MO(MOD)
 
 # Keys
 FNRA_A = KC.RALT(KC.A)
@@ -72,26 +75,7 @@ FNRA_X = KC.RALT(KC.X)
 FNRA_Z = KC.RALT(KC.Z)
 FN_AST = KC.LSFT(KC.N8)
 
-TAPPING_TERM = 157
-SHIFT_TAPPING_TERM = 177
-ALT_TAPPING_TERM = 177
-GUI_TAPPING_TERM = 177
-CTRL_TAPPING_TERM = 177
-
-FN_P = KC.MT(KC.P, KC.LGUI, prefer_hold=False, tap_interrupted=False, tap_time=GUI_TAPPING_TERM)
-FN_G = KC.MT(KC.G, KC.RGUI, prefer_hold=True, tap_interrupted=True, tap_time=GUI_TAPPING_TERM)
-
-FN_DOT = KC.MT(KC.DOT, KC.LALT, prefer_hold=True, tap_interrupted=True, tap_time=ALT_TAPPING_TERM)
-FN_C = KC.MT(KC.C, KC.LALT, prefer_hold=True, tap_interrupted=True, tap_time=ALT_TAPPING_TERM)
-
-FN_COMM = KC.MT(KC.COMM, KC.LCTL, prefer_hold=True, tap_interrupted=True, tap_time=CTRL_TAPPING_TERM)
-FN_R = KC.MT(KC.R, KC.RCTL, prefer_hold=False, tap_interrupted=False, tap_time=CTRL_TAPPING_TERM)
-
-FN_L = KC.MT(KC.L, KC.RSFT, prefer_hold=True, tap_interrupted=True, tap_time=SHIFT_TAPPING_TERM)
-FN_QUOT = KC.MT(KC.QUOT, KC.LSFT, prefer_hold=True, tap_interrupted=True, tap_time=SHIFT_TAPPING_TERM)
-
-FN_J = KC.LT(RALTS, KC.J, prefer_hold=True, tap_interrupted=False, tap_time=TAPPING_TERM)
-
+FN_J = KC.MO(RALTS)
 FN_X = TD_UPPER_LOWER_MOD
 
 HYPER = KC.F9
@@ -101,126 +85,33 @@ secrets = read_secrets()
 MC_SU1 = send_string(secrets[0])
 MC_ROOT = send_string(secrets[1])
 
-RESET = KC.RESET
 XXXXXXX = KC.TRNS
 RGB_TOG = KC.RGB_TOG
 
-KC_F1 = KC.F1
-KC_F2 = KC.F2
-KC_F3 = KC.F3
-KC_F4 = KC.F4
-KC_F5 = KC.F5
-KC_F6 = KC.F6
-KC_F7 = KC.F7
-KC_F8 = KC.F8
-KC_F9 = KC.F9
-KC_F10 = KC.F10
-KC_F11 = KC.F11
-KC_F12 = KC.F12
-
-KC_0 = KC.N0
-KC_1 = KC.N1
-KC_2 = KC.N2
-KC_3 = KC.N3
-KC_4 = KC.N4
-KC_5 = KC.N5
-KC_6 = KC.N6
-KC_7 = KC.N7
-KC_8 = KC.N8
-KC_9 = KC.N9
-
-KC_A = KC.A
-KC_B = KC.B
-KC_D = KC.D
-KC_E = KC.E
-KC_F = KC.F
-KC_H = KC.H
-KC_I = KC.I
-KC_K = KC.K
-KC_M = KC.M
-KC_N = KC.N
-KC_O = KC.O
-KC_Q = KC.Q
-KC_S = KC.S
-KC_T = KC.T
-KC_U = KC.U
-KC_V = KC.V
-KC_W = KC.W
-KC_X = KC.X
-KC_Y = KC.Y
-KC_Z = KC.Z
-
-KC_BSLS = KC.BSLS
-KC_BSPC = KC.BSPC
-KC_INSERT = KC.INSERT
-KC_DELETE = KC.DELETE
-
-KC_CLCK = KC.CLCK
-
-KC_COMM = KC.COMM
-KC_DOT = KC.DOT
-KC_PLUS = KC.PLUS
-KC_MINS = KC.MINS
-KC_EQL = KC.EQL
-KC_PIPE = KC.PIPE
-KC_QUES = KC.QUES
-KC_SLSH = KC.SLSH
-KC_TILD = KC.TILD
-KC_UNDS = KC.UNDS
-
-KC_ENT = KC.ENT
-KC_ESC = KC.ESC
-KC_SPC = KC.SPC
-KC_TAB = KC.TAB
-
-KC_GRV = KC.GRV
-
-KC_HOME = KC.HOME
-KC_END = KC.END
-KC_PGDOWN = KC.PGDOWN
-KC_PGUP = KC.PGUP
-KC_UP = KC.UP
-KC_DOWN = KC.DOWN
-KC_LEFT = KC.LEFT
-KC_RIGHT = KC.RIGHT
-
-KC_LBRC = KC.LBRC
-KC_RBRC = KC.RBRC
-KC_LCBR = KC.LCBR
-KC_RCBR = KC.RCBR
-KC_LPRN = KC.LPRN
-KC_RPRN = KC.RPRN
-
-KC_GT = KC.RABK
-KC_LT = KC.LABK
-
-KC_SCLN = KC.SCLN
-KC_SLCK = KC.SLCK
-
-KC_VOLD = KC.VOLD
-KC_VOLU = KC.VOLU
-KC_MUTE = KC.MUTE
-
 keyboard.keymap = [[
-    FN_QUOT,   FN_COMM,    FN_DOT,      FN_P,      KC_Y,      KC_F,      FN_G,      FN_C,      FN_R,      FN_L,
-       KC_A,      KC_O,      KC_E,      KC_U,      KC_I,      KC_D,      KC_H,      KC_T,      KC_N,      KC_S,
-    KC_SCLN,      KC_Q,      FN_J,      KC_K,      FN_X,    KC_SPC,      KC_M,      KC_W,      KC_V,      KC_Z,
+     FN_MOD,   KC.COMM,    KC.DOT,      KC.P,      KC.Y,      KC.F,      KC.G,      KC.C,      KC.R,      KC.L,
+       KC.A,      KC.O,      KC.E,      KC.U,      KC.I,      KC.D,      KC.H,      KC.T,      KC.N,      KC.S,
+    KC.SCLN,      KC.Q,      FN_J,      KC.K,      FN_X,    KC.SPC,      KC.M,      KC.W,      KC.V,      KC.Z,
 ], [
-    KC_BSLS,   KC_COMM,    KC_DOT,   KC_PIPE,    FN_AST,   KC_QUES,      KC_1,      KC_2,      KC_3,      KC_0,
-     KC_TAB,    KC_EQL,   KC_PLUS,   KC_UNDS,   KC_MINS,   KC_SLSH,      KC_4,      KC_5,      KC_6,   KC_BSPC,
-     KC_ESC,   XXXXXXX,   XXXXXXX,      KC_X,   XXXXXXX,      KC_B,      KC_7,      KC_8,      KC_9,    KC_ENT,
+    KC.BSLS,   KC.COMM,    KC.DOT,   KC.PIPE,    FN_AST,   KC.QUES,     KC.N1,     KC.N2,     KC.N3,     KC.N0,
+     KC.TAB,    KC.EQL,   KC.PLUS,   KC.UNDS,   KC.MINS,   KC.SLSH,     KC.N4,     KC.N5,     KC.N6,   KC.BSPC,
+     KC.ESC,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,     KC.N7,     KC.N8,     KC.N9,    KC.ENT,
 ], [
-  KC_INSERT,    KC_GRV,   KC_TILD,   KC_LBRC,     KC_LT,     KC_GT,   KC_RBRC,    FNRA_C,    XXXXXXX,    FNRA_L,
-     FNRA_A,    FNRA_O,    FNRA_E,   KC_LPRN,   KC_LCBR,   KC_RCBR,   KC_RPRN,     KC_UP,     FNRA_N,    FNRA_S,
-  KC_DELETE,     SUPER,   XXXXXXX,     HYPER,    FNRA_X,      KC_B,   KC_LEFT,   KC_DOWN,   KC_RIGHT,    FNRA_Z,
+  KC.INSERT,    KC.GRV,   KC.TILD,   KC.LBRC,   KC.LABK,   KC.RABK,   KC.RBRC,    FNRA_C,    XXXXXXX,    FNRA_L,
+     FNRA_A,    FNRA_O,    FNRA_E,   KC.LPRN,   KC.LCBR,   KC.RCBR,   KC.RPRN,     KC.UP,     FNRA_N,    FNRA_S,
+  KC.DELETE,     SUPER,   XXXXXXX,     HYPER,    FNRA_X,      KC.B,   KC.LEFT,   KC.DOWN,   KC.RIGHT,    FNRA_Z,
 ], [
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-    KC_CLCK,   KC_SLCK,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_PGUP,   XXXXXXX,   RGB_TOG,
-      RESET,   XXXXXXX,   XXXXXXX,      KC_X,   XXXXXXX,   XXXXXXX,   KC_HOME, KC_PGDOWN,    KC_END,    KC_ENT,
+    KC.LSFT,   KC.LCTL,   KC.LALT,   KC.LGUI,   XXXXXXX,   XXXXXXX,   KC.RGUI,   KC.LALT,   KC.RCTL,   KC.RSFT,
+    KC.CLCK,   KC.SLCK,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC.PGUP,   XXXXXXX,   RGB_TOG,
+   KC.RESET,   XXXXXXX,   XXXXXXX,      KC.X,   XXXXXXX,   XXXXXXX,   KC.HOME, KC.PGDOWN,    KC.END,    KC.ENT,
 ], [
-      KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,    KC_F10,
-    XXXXXXX,   XXXXXXX,   KC_VOLU,   KC_MUTE,    KC_F11,    KC_F12,    MC_SU1,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-    XXXXXXX,   XXXXXXX,   KC_VOLD,   XXXXXXX,   XXXXXXX,   XXXXXXX,   MC_ROOT,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+      KC.F1,     KC.F2,     KC.F3,     KC.F4,     KC.F5,     KC.F6,     KC.F7,     KC.F8,     KC.F9,    KC.F10,
+    XXXXXXX,   XXXXXXX,   KC.VOLU,   KC.MUTE,    KC.F11,    KC.F12,    MC_SU1,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   KC.VOLD,   XXXXXXX,   XXXXXXX,   XXXXXXX,   MC_ROOT,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+], [
+    XXXXXXX,   KC.LCTL,   KC.LALT,   KC.LGUI,   XXXXXXX,   XXXXXXX,   KC.RGUI,   KC.LALT,   KC.RCTL,   KC.RSFT,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   KC.QUOT,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,      KC.J,   XXXXXXX,      KC.X,      KC.B,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
 ]]
 
 keyboard.debug_enabled = True
